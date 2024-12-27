5e49f0f5775fd6d0b4364191bf7904fad98dc0ef
Mohammadsiddiqueps <126324832+Mohammadsiddiqueps@users.noreply.github.com>
2024-12-27 10:27:40 +0530

    created function for seperating args and command

--- shell/my_shell.js
+++ shell/my_shell.js
@@ -1,7 +1,27 @@
 const curerntDirectory = "~";
 
-while (true) {
-  const promptLabel = "My_shell @Mohammads-MacBook-Pro" + curerntDirectory;
-  const command = prompt(promptLabel);
-  console.log(command);
+const fileSystem = {
+  "~": {}
 }
+
+const executeInputCommand = (command, args) => {
+  console.log("command", command);
+  console.log("args", args);
+};
+
+const seperateArgAndCommand = (commandWithArg) => {
+  const words = commandWithArg.split(" ");
+  const command = words.shift();
+
+  return [command, words];
+};
+
+const runMyShell = () => {
+  while (true) {
+    const promptLabel = "My_shell @Mohammads-MacBook-Pro" + curerntDirectory;
+    const inputCommand = prompt(promptLabel);
+    executeInputCommand(...seperateArgAndCommand(inputCommand));
+  }
+};
+
+runMyShell();
