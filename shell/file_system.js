f2bbb9f52ca7b93360090524c4357a20d4a7ab18
Mohammadsiddiqueps <126324832+Mohammadsiddiqueps@users.noreply.github.com>
2024-12-27 15:53:06 +0530

    imported file system to other

---
+++ shell/file_system.js
@@ -0,0 +1,13 @@
+export let curerntDirectory = "~";
+export let currentPath = [];
+
+export const fileSystem = {
+  downloads: {},
+  documents: {},
+  workspace: {
+    javascript: {
+      assigments: {},
+      test: { bla: "" },
+    },
+  },
+};
--- shell/my_shell.js
+++ shell/my_shell.js
@@ -1,12 +1,57 @@
-const curerntDirectory = "~";
+import { fileSystem, curerntDirectory, currentPath } from "./file_system.js";
 
-const fileSystem = {
-  "~": {}
-}
+const runCDCommand = (args) => {
+  const pathToGo = args[0].split("/");
+  const innerFiles = currentPath.reduce(getNestedKey, fileSystem);
+
+  if (!pathToGo.reduce((object, key) => object[key], innerFiles)) {
+    console.log("invalid Path");
+    return;
+  }
+
+  currentPath = [...currentPath, ...pathToGo];
+  curerntDirectory = currentPath.at(-1);
+};
+
+const runLSCommand = (args) => {
+  console.log("ls command", args);
+};
+
+const runMKDIRCommand = (args) => {
+  console.log("MKDIR", args);
+};
+
+const runRMDIRCommand = (args) => {
+  console.log("RMDIR", args);
+};
+
+const runTOUCHCommand = (args) => {
+  console.log("TOUCH", args);
+};
+
+const executeExternalCommand = (commandName, args) => {
+  const externalCommands = {
+    mkdir: runMKDIRCommand,
+    rmdir: runRMDIRCommand,
+    touch: runTOUCHCommand,
+  };
+
+  if (!(commandName in externalCommands)) {
+    console.log(commandName, "- command not found!");
+    return;
+  }
+
+  externalCommands[commandName](args);
+};
 
 const executeInputCommand = (command, args) => {
-  console.log("command", command);
-  console.log("args", args);
+  const internalCommands = { cd: runCDCommand, ls: runLSCommand };
+
+  if (command in internalCommands) {
+    return internalCommands[command](args);
+  }
+
+  return executeExternalCommand(command, args);
 };
 
 const seperateArgAndCommand = (commandWithArg) => {
@@ -18,7 +63,7 @@ const seperateArgAndCommand = (commandWithArg) => {
 
 const runMyShell = () => {
   while (true) {
-    const promptLabel = "My_shell @Mohammads-MacBook-Pro" + curerntDirectory;
+    const promptLabel = "My_shell @Mohammads-MacBook-Pro " + curerntDirectory;
     const inputCommand = prompt(promptLabel);
     executeInputCommand(...seperateArgAndCommand(inputCommand));
   }
