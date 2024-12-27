aba990125c6b3d4d4b76c55475ba84147683c77f
Mohammadsiddiqueps <126324832+Mohammadsiddiqueps@users.noreply.github.com>
2024-12-27 10:10:10 +0530

    creted shell file

---
+++ shell/my_shell.js
@@ -0,0 +1,7 @@
+const curerntDirectory = "~";
+
+while (true) {
+  const promptLabel = "My_shell @Mohammads-MacBook-Pro" + curerntDirectory;
+  const command = prompt(promptLabel);
+  console.log(command);
+}
